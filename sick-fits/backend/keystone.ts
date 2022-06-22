a5df876676ac6ebea1308dae60ca57066e8231d0
sbrownWescoDist <SBrown@wescodist.com>
2022-06-21 20:56:19 -0400

    Updated tutorial files

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,60 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add in initial roles
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({
+      User,
+      Product,
+      ProductImage,
+      // schema items go here
+    }),
+    ui: {
+      // Show UI for only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+    // add session values here
+  })
+);
---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  -@Font-face {
+    font-family: 'radnika_next';
+    src: url(/static/radnikanext-medium-webfont.woff2);
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rbga(0,0,0,0.9)
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+`;
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <GlobalStyles />
+      <Header />
+      <InnerStyles>{children}</InnerStyles>
+    </div>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.any,
+};
